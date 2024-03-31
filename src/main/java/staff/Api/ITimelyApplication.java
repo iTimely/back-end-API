bc14ff33e69273fb7e0d658405c7aae159b05621
NicoleMattes <nicolemattesafonso06@gmail.com>
2024-03-31 20:50:13 -0300

    git commit -m  "upload proect"

---
+++ src/main/java/staff/Api/ITimelyApplication.java
@@ -0,0 +1,13 @@
+package staff.Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITimelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ITimelyApplication.class, args);
+	}
+
+}
